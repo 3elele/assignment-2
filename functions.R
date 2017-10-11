ebbcffc64016605374d72756614d4f44b799e8dc
3elele <gabrielechignoli@icloud.com>
2017-10-11 19:36:30 +0200

    Add function sum_divided_by()

--- functions.R
+++ functions.R
@@ -11,9 +11,9 @@
 sum_column <- function(d, var) {
   result <- NULL # Set a default value to return
   x <- d[[var]] # Access to a column in d
-  if (!is.null(x)) { # Test the existence of the column
-    if (is.numeric(x)) { #Test if x contains numeric values and...
-      sum(x) -> result #...if true we sum all values to get the result
+  if(!is.null(x)) { # Test the existence of the column
+    if(is.numeric(x)) { #Test if x contains numeric values and...
+      my_sum(x) -> result #...if true we sum all values to get the result
     }
   }
   return(result)
@@ -34,12 +34,12 @@ sum_column <- function(d, var) {
 # all values; otherwise, returns NULL
 my_sum <- function(x) {
   result <- NULL #default value to return
-  if (is.numeric(x)) { #X numerical verification...
-    s <- 0 #Establishing a sum variable
-    for(v in 1:length(x)) { #...if true we iterate on its values...
-      s <- s + x[v] #...and we add every value to our result
+  if(is.numeric(x)) { #X numerical verification...
+    future_sum <- 0 #Establishing a sum variable
+    for(v in 1:length(x)) { #...if true we iterate on its length starting from index 1...
+      future_sum <- future_sum + x[v] #...and we add every value to our sum
     }
-    result <- s
+    result <- future_sum #Result become sum
   }
   return(result)
 }
@@ -49,12 +49,24 @@ my_sum <- function(x) {
 ## [1] 1520
 
 #----------1c
-
-#Write a function called sum_divided_by that takes two arguments: a vector x and a number k. It returns the sum of the elements of x divided by the number k. If either x or k are non-numeric, it should return NULL. You can do anything you want, with one requirement: to calculate the sum of the elements in x, you must use your function my_sum. Make sure you write a comment at the top, and, in this case too, write a comment above every line explaining what it does.
-#In order to verify that your function works, add a new chunk to your Rmd that contains the following code:
+# Sum values in a vector and divide the result by a number.
+#
+# ARGUMENTS:
+# x: a vector
+# k: a number
+#
+# RETURN VALUE:
+# if the vector contains numbers, returns the sum of
+# all values divided by the number; otherwise, returns NULL
+sum_divided_by <- function(x, k) {
+  result <- NULL #Initializing the result to NULL
+  if (is.numeric(x) && is.numeric(k) && length(k)==1) { #Verifying if x and k have numeric values and k is just 1 number
+    result <- my_sum(x)/k #Applying my_sum to the vector and dividing by k
+  }
+  return(result)
+}
 #The output should be the following:
   ## [1] 73.04167
   ## NULL
   ## NULL
   ## [1] -126.6667
-#  (Technically, in R, all numbers are vectors that simply happen to be of length one. This means that you could accidentally put a longer vector in place of k, instead of just a number, and you wouldn’t know there was a problem until you looked at the strange result. You don’t need to worry about this problem. Just make sure that your comment at the top of the function explains what k is supposed to be.)
